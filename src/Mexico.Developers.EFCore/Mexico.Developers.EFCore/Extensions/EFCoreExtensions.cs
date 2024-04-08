f12f6c4b401214a56f82ddf871f3761c0d9c1f56
Daniel Sebastiao <daniel@mxdevelopers.net>
2024-04-08 15:40:40 -0700

    Change return type for create a new entity

---
+++ src/Mexico.Developers.EFCore/Mexico.Developers.EFCore/Extensions/EFCoreExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Mexico.Developers.Core.Abstractions;
+
+namespace Mexico.Developers.EFCore.Extensions;
+
+/// <summary>
+/// Provides a set of extension methods for Atos.EFCore
+/// </summary>
+public static class EFCoreExtensions
+{
+    /// <summary>
+    /// Sets the traversal properties of an entity that implements the IEntityBase interface
+    /// </summary>
+    /// <typeparam name="TKey">Type of data that will identify the record</typeparam>
+    /// <typeparam name="TUserKey">Type of data that the user will identify</typeparam>
+    /// <typeparam name="TEntity">The entity type to be configured.</typeparam>
+    /// <param name="userRequired">Default true</param>
+    /// <param name="maxLenghtUser">Default 256</param>
+    /// <param name="tableName">Table from database</param>
+    /// <param name="builder">The builder to be used to configure the entity type.</param>
+    public static void ConfigurationBase<TKey, TUserKey, TEntity>(this EntityTypeBuilder<TEntity> builder, string tableName, bool userRequired = true, int maxLenghtUser = 256)
+        where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        builder.ToTable(tableName);
+
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.UserCreatorId).HasMaxLength(maxLenghtUser).IsRequired(userRequired);
+        builder.Property(x => x.State).IsRequired();
+        builder.Property(x => x.CreatedDate).IsRequired();
+    }
+    /// <summary>
+    /// Gets all repositories and registers them in the.net core dependency container
+    /// </summary>
+    /// <typeparam name="TKey">Type of data that will identify the record</typeparam>
+    /// <typeparam name="TUserKey">Type of data that the user will identify</typeparam>
+    /// <typeparam name="TContext">Represents a session with the database and can be used to query and save instances of your entities</typeparam>
+    /// <param name="services">The IServiceCollection to add services to.</param>
+    public static void AddRepositories<TKey, TUserKey, TContext>(this IServiceCollection services)
+        where TContext : DbContext
+    {
+        var assembly = typeof(TContext).GetTypeInfo().Assembly;
+
+        var @types = assembly.GetTypes().Where(x => !x.IsNested && !x.IsInterface && typeof(IRepositoryBase<TKey, TUserKey>).IsAssignableFrom(x));
+
+        foreach (var type in @types)
+        {
+            var @interface = type.GetInterface($"I{type.Name}", false);
+
+            services.AddTransient(@interface, type);
+        }
+    }
+    /// <summary>
+    /// Obtenga todas las clases que implementan la interfaz IEntityTypeConfiguration{TEntity} y cree una instancia para invocar el método configure
+    /// </summary>
+    /// <typeparam name="TContext">Represents a session with the database and can be used to query and save instances of your entities</typeparam>
+    /// <param name="builder">The builder to be used to configure the entity type.</param>
+    public static void RegisterEntityConfigurations<TContext>(this ModelBuilder builder)
+        where TContext : DbContext
+    {
+        var assembly = typeof(TContext).GetTypeInfo().Assembly;
+
+        var entityConfigurationTypes = assembly.GetTypes().Where(x => IsEntityTypeConfiguration(x));
+
+        var entityMethod = builder.GetMethodEntity();
+
+        foreach (var entityConfigurationType in entityConfigurationTypes)
+        {
+            var genericTypeArgument = entityConfigurationType.GetInterfaces().Single().GenericTypeArguments.Single();
+
+            var genericEntityMethod = entityMethod.MakeGenericMethod(genericTypeArgument);
+
+            var entityTypeBuilder = genericEntityMethod.Invoke(builder, null);
+
+            var instance = Activator.CreateInstance(entityConfigurationType);
+
+            instance.GetType().GetMethod(nameof(IEntityTypeConfiguration<object>.Configure)).Invoke(instance, new[] { entityTypeBuilder });
+        }
+    }
+    /// <summary>
+    /// Method Extension that get the method Entity of the object ModelBuilder
+    /// </summary>
+    /// <param name="builder">Object of type <see cref="ModelBuilder"/></param>
+    /// <returns>Return an object of type <see cref="MethodInfo"/></returns>
+    private static MethodInfo GetMethodEntity(this ModelBuilder builder)
+    {
+        return builder.GetType().GetMethods().Single(x => x.IsGenericMethod && x.Name == nameof(ModelBuilder.Entity) && x.ReturnType.Name == "EntityTypeBuilder`1");
+    }
+    /// <summary>
+    /// Method extension that validates if the type inheritance is IEntityTypeConfiguration
+    /// </summary>
+    /// <param name="type">Type</param>
+    /// <returns>Return true if the type inheritanced is of IEntityTypeConfiguration</returns>
+    private static bool IsEntityTypeConfiguration(Type type)
+    {
+        return type.GetInterfaces().Any(x => x.GetTypeInfo().IsGenericType && x.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>));
+    }
+}
---
+++ src/Mexico.Developers.EFCore/Mexico.Developers.EFCore/Repositories/RepositoryBase.cs
@@ -0,0 +1,140 @@
+﻿using Mexico.Developers.Core.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mexico.Developers.EFCore.Repositories;
+
+/// <summary>
+/// This interface implement the most concurrent methods with the database
+/// </summary>
+public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUserKey>
+{
+    /// <summary>
+    /// Represents a session with the database and can be used to query and save instances of your entities
+    /// </summary>
+    protected readonly DbContext Context;
+
+    /// <summary>
+    /// Create a new instace of Repository
+    /// </summary>
+    /// <param name="context">Represents a session with the database and can be used to query and save instances of your entities</param>
+    /// <exception cref="ArgumentNullException">If context is null</exception>
+    protected RepositoryBase(DbContext context)
+    {
+        this.Context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    /// <summary>
+    /// Method that creates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to create</typeparam>
+    /// <param name="entity">Entity to create</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    public Task<TKey> CreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        if (entity == null)
+            throw new ArgumentNullException(nameof(entity));
+
+        return this.ProcessCreateItemAsync(entity, cancellationToken);
+    }
+    /// <summary>
+    /// Method that creates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to create</typeparam>
+    /// <param name="entity">Entity to create</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    private async Task<TKey> ProcessCreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        await this.Context.AddAsync(entity, cancellationToken);
+        await this.Context.SaveChangesAsync(cancellationToken);
+        return entity.Id;
+
+    }
+    /// <summary>
+    /// Method that deletes an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to delete</typeparam>
+    /// <param name="id">A function to test each element for a condition.</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    public Task<bool> DeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        if (id == null)
+            throw new ArgumentException($"{nameof(id)} can't be null or zero");
+
+        return this.ProcessDeleteAsync<TEntity>(id, cancellationToken);
+    }
+    /// <summary>
+    /// Method that deletes an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to delete</typeparam>
+    /// <param name="id">A function to test each element for a condition.</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    private async Task<bool> ProcessDeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey> 
+    {
+        var entity = await this.Context.Set<TEntity>().FirstOrDefaultAsync( x => (x.Id != null && x.Id.Equals(id)), cancellationToken);
+
+        if (entity != null)
+        {
+            this.Context.Set<TEntity>().Remove(entity);
+
+            return await this.Context.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+        return false;
+    }
+    /// <summary>
+    /// Get a DbSet that can be used to query and save instances of TEntity.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity for which a set should be returned.</typeparam>
+    /// <returns>A set for the given entity type.</returns>
+    public async Task<IEnumerable<TEntity?>> GetAllAsync<TEntity>(bool state, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        return await this.Context.Set<TEntity>().AsNoTracking().Where(x => x.State == state).ToListAsync(cancellationToken);
+    }
+    /// <summary>
+    /// Get a a entity by Id.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<TEntity?> GetAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        return await this.Context.Set<TEntity>().AsNoTracking().Where(x => x.Id != null && x.Id.Equals(id)).FirstOrDefaultAsync(cancellationToken);
+    }
+    /// <summary>
+    /// Method that updates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    public Task<bool> UpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        if (entity == null)
+            throw new ArgumentNullException(nameof(entity));
+
+        return this.ProcessUpdateItemAsync(entity, cancellationToken);
+    }
+    /// <summary>
+    /// Method that updates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    private async Task<bool> ProcessUpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        this.Context.Set<TEntity>().Update(entity);
+
+        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.UserCreatorId)).IsModified = false;
+        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.CreatedDate)).IsModified = false;
+
+        var success = await this.Context.SaveChangesAsync(cancellationToken) > 0;
+
+        return success;
+    }
+}
