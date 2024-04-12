4fd3d9d3af9d9ad9dec48388b48c18ecd7b24a6a
Daniel Sebastiao <daniel@mxdevelopers.net>
2024-04-11 20:54:47 -0700

    Unit Of Work Implementation

--- src/Mexico.Developers.EFCore/Mexico.Developers.EFCore/Repositories/RepositoryBase.cs
+++ src/Mexico.Developers.EFCore/Mexico.Developers.EFCore/Repositories/RepositoryBase.cs
@@ -32,13 +32,14 @@ public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUs
     /// <param name="entity">Entity to create</param>
     /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
     /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    public Task<TKey> CreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    public Task CreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
     {
         if (entity == null)
             throw new ArgumentNullException(nameof(entity));
 
         return this.ProcessCreateItemAsync(entity, cancellationToken);
     }
+
     /// <summary>
     /// Method that creates an entity in the database
     /// </summary>
@@ -46,13 +47,12 @@ public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUs
     /// <param name="entity">Entity to create</param>
     /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
     /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    private async Task<TKey> ProcessCreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    private async Task ProcessCreateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
     {
         await this.Context.AddAsync(entity, cancellationToken);
-        await this.Context.SaveChangesAsync(cancellationToken);
-        return entity.Id;
 
     }
+
     /// <summary>
     /// Method that deletes an entity in the database
     /// </summary>
@@ -60,13 +60,14 @@ public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUs
     /// <param name="id">A function to test each element for a condition.</param>
     /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
     /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    public Task<bool> DeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    public Task DeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
     {
         if (id == null)
             throw new ArgumentException($"{nameof(id)} can't be null or zero");
 
         return this.ProcessDeleteAsync<TEntity>(id, cancellationToken);
     }
+
     /// <summary>
     /// Method that deletes an entity in the database
     /// </summary>
@@ -74,18 +75,43 @@ public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUs
     /// <param name="id">A function to test each element for a condition.</param>
     /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
     /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    private async Task<bool> ProcessDeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey> 
+    private async Task ProcessDeleteAsync<TEntity>(TKey id, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey> 
     {
         var entity = await this.Context.Set<TEntity>().FirstOrDefaultAsync( x => (x.Id != null && x.Id.Equals(id)), cancellationToken);
-
-        if (entity != null)
+        if (entity is null)
         {
-            this.Context.Set<TEntity>().Remove(entity);
-
-            return await this.Context.SaveChangesAsync(cancellationToken) > 0;
+            throw new EntityNotFoundException($"The entity with id {id} not found");
         }
+        this.Context.Set<TEntity>().Remove(entity);
+    }
+    
+    /// <summary>
+    /// Method that updates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    public Task UpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        if (entity == null)
+            throw new ArgumentNullException(nameof(entity));
 
-        return false;
+        return this.ProcessUpdateItemAsync(entity, cancellationToken);
+    }
+    /// <summary>
+    /// Method that updates an entity in the database
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>Represents an asynchronous operation that can return a value.</returns>
+    private Task ProcessUpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
+    {
+        this.Context.Set<TEntity>().Update(entity);
+        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.UserCreatorId)).IsModified = false;
+        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.CreatedDate)).IsModified = false;
+        return Task.CompletedTask;
     }
     /// <summary>
     /// Get a DbSet that can be used to query and save instances of TEntity.
@@ -120,38 +146,5 @@ public abstract class RepositoryBase<TKey, TUserKey> : IRepositoryBase<TKey, TUs
                     throw;
             }
         }
-    }
-
-    /// <summary>
-    /// Method that updates an entity in the database
-    /// </summary>
-    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
-    /// <param name="entity">Entity to update</param>
-    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
-    /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    public Task<bool> UpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
-    {
-        if (entity == null)
-            throw new ArgumentNullException(nameof(entity));
-
-        return this.ProcessUpdateItemAsync(entity, cancellationToken);
-    }
-    /// <summary>
-    /// Method that updates an entity in the database
-    /// </summary>
-    /// <typeparam name="TEntity">Type of the entity to udpate</typeparam>
-    /// <param name="entity">Entity to update</param>
-    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
-    /// <returns>Represents an asynchronous operation that can return a value.</returns>
-    private async Task<bool> ProcessUpdateItemAsync<TEntity>(TEntity entity, CancellationToken cancellationToken) where TEntity : class, IEntityBase<TKey, TUserKey>
-    {
-        this.Context.Set<TEntity>().Update(entity);
-
-        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.UserCreatorId)).IsModified = false;
-        this.Context.Entry(entity).Property(nameof(IEntityBase<TKey, TUserKey>.CreatedDate)).IsModified = false;
-
-        var success = await this.Context.SaveChangesAsync(cancellationToken) > 0;
-
-        return success;
     }
 }
